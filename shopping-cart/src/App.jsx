f22d68d9b76a5143873ce49831d1521bb404060d
Pat <pat_boo61@tbs.tu.ac.th>
2024-06-01 14:54:15 +0700

    Initialize product list with the two given products but use numbers for id

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -5,8 +5,23 @@ import ProductDashboard from './components/ProductDashboard';
 import Cart from './components/Cart';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [id, setId] = useState(0);
+  const [products, setProducts] = useState([
+    {
+      id: 0,
+      name: "Product 1",
+      price: 10.0,
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIXzoYdO9tqmkjlVHmpgXnOsQb9DWkz_Mfi1Jc7zNzaw&s",
+      checked: false,
+    },
+    {
+      id: 1,
+      name: "Product 2",
+      price: 10.0,
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIXzoYdO9tqmkjlVHmpgXnOsQb9DWkz_Mfi1Jc7zNzaw&s",
+      checked: false,
+    }
+  ]);
+  const [id, setId] = useState(2);
   const [cart, setCart] = useState([]);
 
   return (
